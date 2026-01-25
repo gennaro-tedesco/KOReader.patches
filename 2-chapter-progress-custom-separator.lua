9b81f7f7bf873e06026ddf36ac7327fb15077ac9
Gennaro Tedesco <g.tedesco@finlink.de>
2026-01-25 16:26:06 +0100

    fix: remove prefix from custom separator

--- 2-chapter-progress-custom-separator.lua
+++ 2-chapter-progress-custom-separator.lua
@@ -3,7 +3,7 @@ logger.info("Applying custom chapter progress separator patch")
 
 local ReaderFooter = require("apps/reader/modules/readerfooter")
 
-local CHAPTER_SEPARATOR = "|"
+local CHAPTER_SEPARATOR = "  "
 local ORIGINAL_SEPARATOR = " ⁄⁄ "
 
 local orig_getChapterProgress = ReaderFooter.getChapterProgress
@@ -12,7 +12,7 @@ function ReaderFooter:getChapterProgress(get_percentage, pageno)
 	local result = orig_getChapterProgress(self, get_percentage, pageno)
 
 	if type(result) == "string" then
-		result = " " .. result:gsub(ORIGINAL_SEPARATOR, CHAPTER_SEPARATOR, 1)
+		result = result:gsub(ORIGINAL_SEPARATOR, CHAPTER_SEPARATOR, 1)
 	end
 
 	return result
